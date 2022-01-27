043fb167f14700e9a764ab3dd452d0ca6288095e
John Carter <cadsoft.dev@gmail.com>
2022-01-27 16:03:53 +0900

    Update [image-id].tsx

--- pages/exhibition/[image-id].tsx
+++ pages/exhibition/[image-id].tsx
@@ -1,7 +1,7 @@
 // exibition/:image-id
 import { useRouter } from 'next/router';
 import React, { useState } from "react";
-import { Box, Center, Container, Text, Wrap, WrapItem, Stack, HStack, VStack, Button, ButtonGroup } from "@chakra-ui/react";
+import { Box, Center, Container, Text, Wrap, WrapItem, Flex, Stack, HStack, VStack, Button, ButtonGroup } from "@chakra-ui/react";
 import Image from "next/image";
 import {getCuratedPhotos} from "../../lib/api";
 
@@ -22,29 +22,33 @@ export default function IndexedImage(data) {
   return (
     <div>
       <Box overflow="hidden" bg="purple.100" minH="100vh">
-        <Container maxW='container.xl' justify="center">
+        <Container maxW='container.xl' mt={40}>
           <VStack>
-            <HStack>
+            <HStack width={800}>
               <Text>{photo.data.title}</Text>
               <Text fontSize='xs' my="1"> {photo.data.aic_start_at.substr(0, 10)} -  {photo.data.aic_end_at ? photo.data.aic_end_at.substr(0, 10) : "Present"}</Text> 
             </HStack>
-            <HStack>
-              <Wrap px="1rem" spacing={4}>
-                <WrapItem
-                  key={photo.data.id}
-                  boxShadow="base"
-                  overflow="hidden"
-                  _hover={{ boxShadow: "dark-lg" }}
-                >
-                  <VStack spacing='24px'>
-                    <Image my="1" loader={myLoader} src={`/iiif/2/${photo.data.image_id}/full/843,/0/default.jpg`} height={200} width={200} alt={photo.data.id} />
-                    </VStack>               
-                </WrapItem>
-              </Wrap> 
-              <Text width={400}>
-                {photo.data.description}
-              </Text>
-            </HStack>
+            <Flex>
+              <Box>
+                <Wrap px="1rem" spacing={4}>
+                  <WrapItem
+                    key={photo.data.id}
+                    boxShadow="base"
+                    overflow="hidden"
+                    _hover={{ boxShadow: "dark-lg" }}
+                  >
+                    <VStack spacing='24px'>
+                      <Image my="1" loader={myLoader} src={`/iiif/2/${photo.data.image_id}/full/843,/0/default.jpg`} height={200} width={200} alt={photo.data.id} />
+                      </VStack>               
+                  </WrapItem>
+                </Wrap> 
+              </Box>
+              <Box>
+                <Text width={600}>
+                  {photo.data.description}
+                </Text>
+              </Box>
+            </Flex>
           </VStack>  
         </Container>
       </Box>      
